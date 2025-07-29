5f7c481c5e20e8f80fa655af1458515e664225f1
Joel Åkergren <joel.akergren@gmail.com>
2025-07-29 21:24:21 +0200

    Add centering of loading message. Make NotFoundView look better.

--- src/views/NotFoundView.tsx
+++ src/views/NotFoundView.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 const NotFoundView = () => {
   return (
-    <div className="not-found">
-      <h1>Oops, sidan kunde inte hittas</h1>
-      <Link to="/start">Tillbaka till Startsidan</Link>
+    <div className="main-container">
+      <div className="not-found">
+        <h1>Oops, sidan kunde inte hittas</h1>
+        <Link to="/start">Tillbaka till Startsidan</Link>
+      </div>
     </div>
   );
 };
